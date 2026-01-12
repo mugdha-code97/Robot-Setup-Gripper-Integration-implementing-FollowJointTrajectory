a0b67465333d0ed53bf8dc196cd2243b99b265fa
mugdha-code97 <mugdhaudawant97@gmail.com>
2026-01-12 23:46:49 +0530

    Create view_robot.launch.py

---
+++ view_robot.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    pkg_path = get_package_share_directory('fanuc_description')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'fanuc_crx10ia.urdf.xacro')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': Command(['xacro ', urdf_file])
+            }]
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            output='screen'
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen'
+        )
+    ])
